f41f076a77c625a5c39d8903a33e649cf87139d9
Rachel0710 <yeeshim1992@hotmail.com>
2021-02-21 02:03:50 +0800

    YS

---
+++ app/src/main/java/com/example/goldenbeachhotelmanagementsystem/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.goldenbeachhotelmanagementsystem
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.widget.Toast
+private const val SPLASH_SCREEN = 5000
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler().postDelayed({
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }, 3000)
+    }
+}
