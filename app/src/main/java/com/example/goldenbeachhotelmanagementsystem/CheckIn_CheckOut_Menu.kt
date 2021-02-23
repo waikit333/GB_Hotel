7f5390e42c959e36d4805447e817c1741bdb4ba5
Wai Kit <chongwk-wm18@student.tarc.edu.my>
2021-02-24 01:56:00 +0800

    Updated home, added navigation

---
+++ app/src/main/java/com/example/goldenbeachhotelmanagementsystem/CheckIn_CheckOut_Menu.kt
@@ -0,0 +1,11 @@
+package com.example.goldenbeachhotelmanagementsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckIn_CheckOut_Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.checkin_checkout_menu)
+    }
+}
--- app/src/main/java/com/example/goldenbeachhotelmanagementsystem/Home.kt
+++ app/src/main/java/com/example/goldenbeachhotelmanagementsystem/Home.kt
@@ -1,11 +1,22 @@
 package com.example.goldenbeachhotelmanagementsystem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //add listener for checkincheckout button
+        val checkInButton: Button = findViewById(R.id.checkInBtn)
+        checkInButton.setOnClickListener{
+            val intent = Intent(this,CheckIn_CheckOut_Menu::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
