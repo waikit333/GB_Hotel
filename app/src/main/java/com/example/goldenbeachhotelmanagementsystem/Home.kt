a18ae590b4f0ac340338b77b4f5218d88d089320
Rachel0710 <yeeshim1992@hotmail.com>
2021-02-25 02:35:01 +0800

    Added home page, actionbar and down toolbar

--- app/src/main/java/com/example/goldenbeachhotelmanagementsystem/Home.kt
+++ app/src/main/java/com/example/goldenbeachhotelmanagementsystem/Home.kt
@@ -8,14 +8,14 @@ import android.widget.Button
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.home)
 
         //add listener for checkincheckout button
-        val checkInButton: Button = findViewById(R.id.checkInBtn)
-        checkInButton.setOnClickListener{
-            val intent = Intent(this,CheckIn_CheckOut_Menu::class.java)
-            startActivity(intent)
-        }
+        //val checkInButton: Button = findViewById(R.id.checkInBtn)
+        //checkInButton.setOnClickListener{
+            //val intent = Intent(this,CheckIn_CheckOut_Menu::class.java)
+            //startActivity(intent)
+        //}
 
 
     }
