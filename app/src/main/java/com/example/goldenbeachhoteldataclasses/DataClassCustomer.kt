f6560e4d7bf82b587d2414bff3c2ed1caa7309f9
Wai Kit <chongwk-wm18@student.tarc.edu.my>
2021-03-08 13:58:35 +0800

    Added data class

--- app/src/main/java/com/example/goldenbeachhoteldataclasses/DataClassCustomer.kt
+++ app/src/main/java/com/example/goldenbeachhoteldataclasses/DataClassCustomer.kt
@@ -1,3 +1,3 @@
-package com.example.goldenbeachhotelmanagementsystem
+package com.example.goldenbeachhoteldataclasses
 
 data class DataClassCustomer(var email:String, val firstName: String, val lastName: String, var phone:String, var ic:String)
--- app/src/main/java/com/example/goldenbeachhoteldataclasses/DataClassRoom.kt
+++ app/src/main/java/com/example/goldenbeachhoteldataclasses/DataClassRoom.kt
@@ -1,4 +1,4 @@
-package com.example.goldenbeachhotelmanagementsystem
+package com.example.goldenbeachhoteldataclasses
 
 data class DataClassRoom(val floor: Int, val room:Int, var price:Double, val type:String, var status:String )
 
